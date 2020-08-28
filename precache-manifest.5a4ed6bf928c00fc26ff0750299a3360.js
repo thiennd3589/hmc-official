45e3be41d2ab83ff4fe1d5c98d113e4ca12119d3
thiennd3589 <thien.nd3589@gmail.com>
2020-08-28 21:02:31 +0700

    Updates

---
+++ precache-manifest.5a4ed6bf928c00fc26ff0750299a3360.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6472e96573a0669d88c6cfdffaf9392",
+    "url": "/hmc-official/index.html"
+  },
+  {
+    "revision": "ed6c4e5bdea964758f0e",
+    "url": "/hmc-official/static/css/main.5dc833ce.chunk.css"
+  },
+  {
+    "revision": "a3371a39adde3b7918e8",
+    "url": "/hmc-official/static/js/2.12c4a0af.chunk.js"
+  },
+  {
+    "revision": "3e36771dec884e943748341c99dc97e1",
+    "url": "/hmc-official/static/js/2.12c4a0af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed6c4e5bdea964758f0e",
+    "url": "/hmc-official/static/js/main.ca548c9c.chunk.js"
+  },
+  {
+    "revision": "af325506f13496f75514",
+    "url": "/hmc-official/static/js/runtime-main.b9891deb.js"
+  },
+  {
+    "revision": "eaa0288f23dc034053274c0c49170eed",
+    "url": "/hmc-official/static/media/HMCWeb_BG_Red.eaa0288f.jpg"
+  },
+  {
+    "revision": "0051f7866e5714f1be53da129fc161dc",
+    "url": "/hmc-official/static/media/HMCWeb_BG_White.0051f786.jpg"
+  },
+  {
+    "revision": "37ccb4c7cd33cbb47d2e2b2f52aeff78",
+    "url": "/hmc-official/static/media/HMCWeb_Icon_01.37ccb4c7.png"
+  },
+  {
+    "revision": "f650b89c6223326dfd11bbcf7bef69d4",
+    "url": "/hmc-official/static/media/HMCWeb_Icon_02.f650b89c.png"
+  },
+  {
+    "revision": "562f11e5aa9b6be3d26608794d221b50",
+    "url": "/hmc-official/static/media/HMCWeb_Icon_03.562f11e5.png"
+  },
+  {
+    "revision": "2270ee5680bfcd6b66456cbc7d64ddd9",
+    "url": "/hmc-official/static/media/img1.2270ee56.jpg"
+  },
+  {
+    "revision": "606005f42c1f10f3195390513ae393b3",
+    "url": "/hmc-official/static/media/img1.606005f4.jpg"
+  },
+  {
+    "revision": "b8b5036e516a53f7042bf5350b52d51b",
+    "url": "/hmc-official/static/media/img1.b8b5036e.jpg"
+  },
+  {
+    "revision": "2ffb78a2cb02761f7daf8a038de0df1a",
+    "url": "/hmc-official/static/media/img2.2ffb78a2.jpg"
+  },
+  {
+    "revision": "41cefd2f6f81cb791fa55a494fbbe6d6",
+    "url": "/hmc-official/static/media/img2.41cefd2f.jpg"
+  },
+  {
+    "revision": "8e00852c8502bb9af3a7fd06c7f0250d",
+    "url": "/hmc-official/static/media/img2.8e00852c.jpg"
+  },
+  {
+    "revision": "0b6d4f6b313411020f087d30f2ed8ea2",
+    "url": "/hmc-official/static/media/img3.0b6d4f6b.jpg"
+  },
+  {
+    "revision": "9dfb00c3a0dab785e210e3b9d5ef9701",
+    "url": "/hmc-official/static/media/img3.9dfb00c3.jpg"
+  },
+  {
+    "revision": "dbd6022f8328b70f7ddf346f5acf2d3c",
+    "url": "/hmc-official/static/media/img3.dbd6022f.jpg"
+  },
+  {
+    "revision": "6938128540a32c64dcc323965fc725fd",
+    "url": "/hmc-official/static/media/img4.69381285.jpg"
+  },
+  {
+    "revision": "796728c30bb337694127ceec9be99031",
+    "url": "/hmc-official/static/media/logo.796728c3.png"
+  }
+]);
